bff8b1728c27a6f8e894e871f8760538d8b097fe
Mario César <mariocesar.c50@gmail.com>
2019-11-20 16:16:15 -0400

    Fix entrypoint call

--- htmlmetadata/__main__.py
+++ htmlmetadata/__main__.py
@@ -3,10 +3,15 @@ import json
 
 from htmlmetadata import extract_metadata
 
-if __name__ == "__main__":
+
+def main():
     parser = argparse.ArgumentParser(description="Extract HTML metadata")
     parser.add_argument("url", help="URL to fetch and extract metadata")
     args = parser.parse_args()
 
     data = extract_metadata(args.url)
     print(json.dumps(data, indent=2))
+
+
+if __name__ == "__main__":
+    main()
